3425f42e01f4cc48cb268162b9d646dae6095a96
mathews225 <petemathews14@gmail.com>
2021-02-19 12:05:27 -0500

    Add project files.

---
+++ Bowling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bowling {
+	class Program {
+		static void Main(string[] args) {
+
+
+			// generate 10 random frames (0-30) of bowling and print the final score.
+
+			var rnd = new Random();
+			var score = new List<int>(10);
+			var final = 0;
+			var msg = $"Scoreboard [";
+
+
+			for (int i = 0; i < 10; i++) {
+				var nbr = rnd.Next(0, 31);
+				// score.Add(nbr); not needed?
+				final += nbr;
+				
+				msg += i != 9 ? $"{nbr}, " : $"{nbr}]";
+			}
+
+			Console.WriteLine($"Final score is {final}.");
+			Console.WriteLine(msg);
+		}
+	}
+}
